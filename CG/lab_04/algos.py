36f70f82eae3dcc7edcd04e53a374e5e1f76c0de
rollingbolling <dan.kazaryan@gmail.com>
2023-05-17 07:54:05 +0300

    brez alg added

---
+++ CG/lab_04/algos.py
@@ -0,0 +1,6 @@
+from brezenhem import brezenhem_circle, brezenhem_ellipse
+from canonic import canonic_circle, canonic_ellipse
+from parametric import parametric_circle, parametric_ellipse
+from midpoint import midpoint_circle, midpoint_ellipse
+
+
--- CG/lab_04/brezenhem.py
+++ CG/lab_04/brezenhem.py
@@ -34,3 +34,41 @@ def brezenhem_circle(canvas, xc, yc, r, colour, draw):
 
         if draw:
             draw_pixels(canvas, [x + xc, y + yc, colour], xc, yc, circle=True)
+
+def brezenhem_ellipse(canvas, xc, yc ,ra, rb, colour, draw):
+    x = 0
+    y = rb
+
+    if draw:
+        draw_pixels(canvas, [x + xc, y + yc, colour], xc, yc, circle=True)
+
+    sqr_ra = ra ** 2
+    sqr_rb = rb ** 2
+    delta =  sqr_rb - sqr_ra * (2 * rb + 1)
+
+    while y >= 0:
+        if delta < 0:
+            d1 = 2 * delta + sqr_ra * (2 * y + 2)
+
+            x += 1
+            if d1 < 0:
+                delta += sqr_rb * (2 * x + 1)
+            else:
+                y -= 1
+                delta += sqr_rb * (2 * x + 1) + sqr_ra * (1 - 2 * y)
+        elif delta > 0:
+            d2 = 2 * delta + sqr_rb * (2 - 2 * x)
+
+            y -= 1
+            if d2 > 0:
+                delta += sqr_ra * (1 - 2 * y)
+            else:
+                x += 1
+                delta += sqr_rb * (2 * x + 1) + sqr_ra * (1 - 2 * y)
+        else:
+            y -= 1
+            x += 1
+            delta += sqr_rb * (2 * x + 1) + sqr_ra * (1 - 2 * y)
+
+        if draw:
+            draw_pixels(canvas, [x + xc, y + yc, colour], xc, yc, circle=False)
