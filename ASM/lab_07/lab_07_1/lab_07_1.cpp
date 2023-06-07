6903b9cca0ac2d987d92bcac91eff3b1a5b8c6e7
rollingbolling <dan.kazaryan@gmail.com>
2023-06-07 13:34:56 +0300

    7-th added

---
+++ ASM/lab_07/lab_07_1/lab_07_1.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+extern "C" {
+    void my_strcpy(char* dst, const char* src, int len);
+}
+
+size_t my_strlen(const char* str) {
+    size_t result = 0;
+    while (*(str++) != 0)
+        result++;
+    return result;
+}
+
+size_t asm_strlen(const char* str) {
+    size_t len = 0;
+    __asm {
+            push ecx
+            push edi
+            mov ecx, -1
+            mov edi, str
+            xor al, al
+            repne scasb
+        not ecx
+            dec ecx
+            mov len, ecx
+            pop edi
+            pop ecx
+    }
+    return len;
+}
+
+int main()
+{
+    char str[100] = "Test strlen for lab_07 assambler!";
+    printf("Test string: %s\n", str);
+    int l = asm_strlen(str);
+    cout << "asm_strlen: " << l << endl;
+    l = my_strlen(str);
+    cout << "my_strlen: " << l << endl;
+    l = strlen(str);
+    cout << "strlen: " << l << endl;
+
+    char src[] = "test strcpy for lab_07 asm";
+    char dst[] = "test strcpy for lab_07 asm";
+    int len = asm_strlen(src);
+
+    printf("Test string: %s\n", src);
+    my_strcpy(dst, src, len);
+    printf("Identic strings: %s\n", dst);
+
+    char before[100] = { '0' },
+        * middle = before + 2,
+        * after = middle + 2;
+
+    printf("String length = %d\n", len);
+
+    my_strcpy(middle, src, len);
+    printf("Coppied string: %s\n", middle);
+
+    my_strcpy(before, middle, len);
+    printf("Copy before pointer: %s\n", before);
+
+    my_strcpy(after, before, len);
+    printf("Copy after pointer: %s\n", before);
+
+
+    return 0;
+}
